1d1eb34940aae8434c8d2bf129c3aea594b3e2b5
FelipeZS <fzsaldanha@hotmail.com>
2020-09-12 12:49:00 -0300

    building listing page

--- challenge/src/pages/Listing/index.js
+++ challenge/src/pages/Listing/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/grupoa.png';
+
+import "./styles.css";
+
 function Listing() {
   return (
-    <div id="page-Listing">
-      
+    <div className="page-listing">
+      <div className="logo-listing"> 
+        <img src={logoImg} alt="logo"/>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
